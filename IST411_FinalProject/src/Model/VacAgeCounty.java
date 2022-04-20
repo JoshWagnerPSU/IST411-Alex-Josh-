0a853a8a1d7d931e3abc91656dd939f007e00107
Alex <akoontz21@gmail.com>
2022-04-20 11:22:44 -0400

    Initializing the Model package, and a few files

---
+++ IST411_FinalProject/src/Model/VacAgeCounty.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author akoon
+ */
+public class VacAgeCounty {
+    
+    private String county,
+            partially_covered_age_group_5_9,
+            partially_covered_age_group_10_14,
+            partiallycovered_15_19,
+            partiallycovered_20_24,
+            partiallycovered_25_29,
+            partiallycovered_30_34,
+            partiallycovered_35_39,
+            partiallycovered_40_44,
+            partiallycovered_45_49,
+            partiallycovered_50_54,
+            partiallycovered_55_59,
+            partiallycovered_60_64,
+            partiallycovered_65_69,
+            partiallycovered_70_74,
+            partiallycovered_75_79,
+            partiallycovered_80_84,
+            partiallycovered_85_89,
+            partiallycovered_70_74,
+            partiallycovered_75_79,
+    
+}
